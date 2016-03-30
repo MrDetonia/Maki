ed009052a6cb58d497c89afc8833f0aaf10ce601
Zac Herd <mrdetonia@gmail.com>
2016-03-30 22:59:51 +0100

    Stopped source link expanding when posted, v0.4.1

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ from secret import email,pwd
 name = "Maki"
 
 # bot version
-version = "v0.4"
+version = "v0.4.1"
 
 # text shown by .help command
 helptext = """I am a bot written in Python by MrDetonia
@@ -102,7 +102,7 @@ def on_message(message):
 
         elif message.content.startswith('.source'):
             # link to source code
-            yield from client.send_message(message.channel, 'These are my insides: http://27b-a.xyz/mrdetonia/Maki')
+            yield from client.send_message(message.channel, 'These are my insides: <http://27b-a.xyz/mrdetonia/Maki>')
 
         elif message.content.startswith('.die') and message.author.id in admins:
             # exit discord and kill bot
